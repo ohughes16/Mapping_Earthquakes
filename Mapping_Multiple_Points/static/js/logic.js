c9af4c6cf5f145daf305b996d6844a1f1f0d9241
ohughes16 <ohughes16@gmail.com>
2020-12-12 21:11:31 -0800

    finishing changes for multiple point map in 13.4.2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -15,9 +15,13 @@ let map = L.map("mapid", {
 let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
+cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location).addTo(map)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
  });
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
